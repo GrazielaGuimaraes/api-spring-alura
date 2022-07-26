c794100937a80000a63ae80db0d6aae4d7141b14
Graziela Guimarães <66334943+GrazielaGuimaraes@users.noreply.github.com>
2022-07-25 23:10:07 -0300

    Create ApiracasApplication.java

---
+++ ApiracasApplication.java
@@ -0,0 +1,17 @@
+package br.com.apiracas.apiracas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiracasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiracasApplication.class, args);
+
+
+
+
+
+
+
